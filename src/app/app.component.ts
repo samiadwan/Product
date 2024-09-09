601e5c1935e4e8b68a86a6c84db69cc36e830390
samiadewan <samia.dewan@brainstation-23.com>
2024-09-09 19:59:11 +0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'product';
+  // title = 'product';
+  productList: any [] =[];
+  product: any [] =[];
+  constructor(private http : HttpClient) { }
+
+   getProducts()
+  {
+    this.http.get("https://dummyjson.com/products").subscribe((result:any)=>{
+      // debugger;
+        this.productList = result.products;
+    }); 
+  }
+
+  getProduct(productId: number)
+  {
+    this.http.get("https://dummyjson.com/products/1").subscribe((result:any)=>{
+        this.product = result;
+        debugger;
+    }); 
+  }
 }
+
